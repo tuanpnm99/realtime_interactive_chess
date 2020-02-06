9637fe6687bbd2cf8bbd762334466f70e81af469
Tuan Phan Nguyen <tuanpnm99@gmail.com>
2020-02-05 20:00:12 -0500

    fix queen move

--- chess.js
+++ chess.js
@@ -8,7 +8,7 @@ class Chess{
     this.removed_pieces_p2 = new Set();
     this.current_pieces = new Set();
     this.p1_turn = true;
-    
+
     pieces.forEach((piece) => {
       this.add_piece(piece);
     });
@@ -228,7 +228,7 @@ class ChessRules{
     var moves_up = ChessRules.move_to_direction(-1, 0, piece, board);
     var moves_down = ChessRules.move_to_direction(1, 0, piece, board);
     var moves_left = ChessRules.move_to_direction(0, -1, piece, board);
-    var moves_right = ChessRules.move_to_direction(-1, 0, piece, board);
+    var moves_right = ChessRules.move_to_direction(0, 1, piece, board);
     var moves_up_left = ChessRules.move_to_direction(-1, -1, piece, board);
     var moves_up_right = ChessRules.move_to_direction(-1, 1, piece, board);
     var moves_down_left = ChessRules.move_to_direction(1, -1, piece, board);
